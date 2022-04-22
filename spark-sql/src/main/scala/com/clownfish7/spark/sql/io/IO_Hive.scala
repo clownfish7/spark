0bf38b32bdba283b4940b92812e946125ac65d1a
clownfish7 <279505647@qq.com>
2022-04-22 15:43:21 +0800

    sql

---
+++ spark-sql/src/main/scala/com/clownfish7/spark/sql/io/IO_Hive.scala
@@ -0,0 +1,27 @@
+package com.clownfish7.spark.sql.io
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import java.util.Properties
+
+/**
+ * classname IO_Hive
+ * description TODO
+ * create 2022-04-22 15:27 
+ */
+object IO_Hive extends App {
+  val sparkConf = new SparkConf().setMaster("local[*]").setAppName("sql")
+  val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+
+  import sparkSession.implicits._
+
+  // 使用 SparkSql 连接外置 hive
+  // 1. 拷贝 hive-site.xml 到 classpath
+  // 2. 启用 hive 支持
+  // 3. 增加对应的依赖关系  包含 mysql 驱动
+
+  sparkSession.sql("show tables").show()
+
+  sparkSession.close()
+}
